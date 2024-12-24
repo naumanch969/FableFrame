7520b1ceac9902c619d0d4711b0f8ad7d1a88587
naumanch969 <naumanch969@gmail.com>
2024-12-24 15:44:27 +0500

    Build fix

--- features/report/api/useGetReports.ts
+++ features/report/api/useGetReports.ts
@@ -2,7 +2,7 @@ import { api } from "@/convex/_generated/api"
 import { useQuery } from "convex/react"
 
 export const useGetReports = () => {
-    const data = useQuery(api.reports.get)
+    const data = useQuery(api.report.get)
 
     const isLoading = data == undefined
 
