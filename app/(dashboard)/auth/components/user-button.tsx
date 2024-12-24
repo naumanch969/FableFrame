9220a0124b7ada59fd2ff2c0f15759c2c45a2946
naumanch969 <naumanch969@gmail.com>
2024-12-24 15:21:43 +0500

    Build fix

--- app/(dashboard)/auth/components/user-button.tsx
+++ app/(dashboard)/auth/components/user-button.tsx
@@ -8,7 +8,7 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
-import { useCurrentUser } from '../api/useCurrentUser'
+import { useCurrentUser } from '@/features/users/api/useCurrentUser'
 import { Loader, LogOut } from 'lucide-react'
 import { useRouter } from 'next/navigation'
 import { useAuthActions } from '@convex-dev/auth/react'
